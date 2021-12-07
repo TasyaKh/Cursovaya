360159fdc8b65632539dbe823c23576813c1d696
tasya132002@gmail.com <tasya132002@gmail.com>
2021-12-07 10:54:05 +0800

    Добавили переменную, которая контролирует изменение размера палочки вектора направления частицы(из за гравитации координата по у все время растет, устранен баг)

--- Cursovaya/Particle.cs
+++ Cursovaya/Particle.cs
@@ -4,32 +4,28 @@ using System.Drawing;
 
 namespace Cursovaya
 {
-    class Particle
+    class Particle:Circle
     {
         //public bool watch_Info;
 
-        public int radius;
-        public float X;
-        public float Y;
-
+   
         public float speedX;
         public float speedY;
+       // public float speedScroller;  //Скроллер скорости замедления
 
         public float life; //Количество жизней
 
-        public static Random rand = new Random();
-        public Color clr;
-
         public Particle()
-        {
-            radius = 2 + rand.Next(10);
-            life = -1;
-            // clr = Color.FromArgb(100+rand.Next(156), 100 + rand.Next(156), 100 + rand.Next(156));
+        { 
+            radius = 5 + rand.Next(15);
+            life = 0;
+          
             clr = Color.White;
-
+            // speedScroller = 1f;
+            // clr = Color.FromArgb(100+rand.Next(156), 100 + rand.Next(156), 100 + rand.Next(156));
             //watch_Info = false;
         }
-        public void Draw(Graphics g) //Рисовать
+        public override void Draw(Graphics g) //Рисовать
         {
             float k = Math.Min(1f, life / 100); //Найти минимальное занчение среди текущей жизни и 1
             int alpha = (int)(k * 255);         //Умножить это знаечние на 255
@@ -37,7 +33,17 @@ namespace Cursovaya
             var color = Color.FromArgb(alpha,clr);
             var b = new SolidBrush(color);
             g.FillEllipse(b, X - radius, Y - radius, radius * 2, radius * 2);
-            g.DrawLine(new Pen(Color.Blue,2), X, Y, X + speedX, Y + speedY);
+
+            var y2 = Y + speedY;
+            float d = (float)Math.Sqrt(Math.Pow(-speedX,2) + Math.Pow(-speedY, 2));
+            if (d > radius)
+               y2 -= (d-radius);
+            else
+            {
+                y2 = Y+radius;
+            }
+
+            g.DrawLine(new Pen(Color.Blue,2), X, Y, X + speedX, y2);
 
           
             b.Dispose();
