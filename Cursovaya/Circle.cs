f6851e6eb0b934a8f4c07f603eab1b39baec8070
tasya132002@gmail.com <tasya132002@gmail.com>
2021-12-07 10:49:46 +0800

    Оющий класс для фигур типа Circle

---
+++ Cursovaya/Circle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cursovaya
+{
+    abstract class Circle
+    {
+        public int radius;
+        public float X;
+        public float Y;
+
+        public static Random rand = new Random();
+        public Color clr;
+
+        public Circle()
+        {
+            radius = 0;
+            X = Y = 0;
+            clr = Color.White;
+        }
+
+        public abstract void Draw(Graphics g);
+    }
+}
