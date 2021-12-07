56cf9f7377a6cfcebaaa048c2b5039c4df1f4cff
tasya132002@gmail.com <tasya132002@gmail.com>
2021-12-07 10:50:49 +0800

    Фигура, при попадании частицы на которую проимходит увеличесние счутчика и обнуление частицы (а так же с учетом заполненности этого сборщика меняем насыщенность его цвета)

---
+++ Cursovaya/CircleCollector.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cursovaya
+{
+    class CircleCollector:Circle
+    {
+        public int countHit;
+        public CircleCollector(int mouseX, int mouseY)
+        {
+            countHit = 0;
+
+            X = mouseX;
+            Y = mouseY;
+            radius = 20; 
+
+            clr = Color.FromArgb(0,Color.Yellow);
+        }
+
+        public override void Draw(Graphics g)
+        {
+            var k = Math.Min(1f,(float)countHit / 500);
+            int alpha = (int)(k * 255);         //Умножить это знаечние на 255
+
+            clr = Color.FromArgb(alpha, Color.Yellow);
+
+            var b = new SolidBrush(clr);
+            g.FillEllipse(b, X - radius, Y - radius, radius * 2, radius * 2);
+            g.DrawEllipse(new Pen(Color.FromArgb(255, 0, 0), 4), X - radius, Y - radius, radius * 2, radius * 2);
+
+            b = new SolidBrush(Color.White);
+            g.DrawString(Convert.ToString(countHit), new Font("Times New Roman", 12), b, X-radius/2 , Y-radius / 2);
+            b.Dispose();
+        }
+    }
+}
