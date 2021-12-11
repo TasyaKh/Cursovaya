ff51fdb8eaaeb0d28d440505e7da520c6dd6c3c9
tasya132002@gmail.com <tasya132002@gmail.com>
2021-12-11 21:01:00 +0800

    Если пересечемся с этим кругом, то частица поменчет цвет

---
+++ Cursovaya/ColorfulCircle.cs
@@ -0,0 +1,21 @@
+﻿using System.Drawing;
+
+namespace Cursovaya
+{
+    class ColorfulCircle : Circle
+    {
+        public ColorfulCircle(float mouseX, float mouseY)
+        {
+            radius = 20;
+            this.X = mouseX;
+            this.Y = mouseY;
+
+            //clr = createNewColor();
+        }
+       
+        public override void Draw(Graphics g)
+        {
+            g.DrawEllipse(new Pen(clr,2), X - radius, Y - radius, radius * 2, radius * 2);
+        }
+    }
+}
