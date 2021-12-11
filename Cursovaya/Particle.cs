8fcad18a5449e34cbd4ccb714af1a4ca1be120cb
tasya132002@gmail.com <tasya132002@gmail.com>
2021-12-11 21:02:28 +0800

    Вынесли корректировку размера палочки вектора движения в отдельный метод вектор

--- Cursovaya/Particle.cs
+++ Cursovaya/Particle.cs
@@ -11,7 +11,6 @@ namespace Cursovaya
    
         public float speedX;
         public float speedY;
-       // public float speedScroller;  //Скроллер скорости замедления
 
         public float life; //Количество жизней
 
@@ -21,44 +20,55 @@ namespace Cursovaya
             life = 0;
           
             clr = Color.White;
-            // speedScroller = 1f;
-            // clr = Color.FromArgb(100+rand.Next(156), 100 + rand.Next(156), 100 + rand.Next(156));
-            //watch_Info = false;
         }
-        public override void Draw(Graphics g) //Рисовать
+
+        public void setColor(Color color)
         {
-            float k = Math.Min(1f, life / 100); //Найти минимальное занчение среди текущей жизни и 1
+            clr = color;
+        }
+        private float vectorWithoutGravitation()
+        {
+            var y2 = Y + speedY;               //Послудующая точка(где перерисуется частица)
+            float d = (float)Math.Sqrt(Math.Pow(-speedX, 2) + Math.Pow(-speedY, 2)); //Найти расстояние до этой точки от текущего положения частицы
+
+            if (d > radius)      //Если расстояние больше радиуса,то
+                y2 -= (d - radius); //вектор движения частиц будет по размеру равен радиусу
+            else
+            {
+                y2 = Y + radius;
+            }
+            return y2;
+        }
+        public override void Draw(Graphics g)   //Рисовать
+        {
+            float k = Math.Min(1f, life / 200); //Найти минимальное занчение среди текущей жизни и 1
             int alpha = (int)(k * 255);         //Умножить это знаечние на 255
 
             var color = Color.FromArgb(alpha,clr);
             var b = new SolidBrush(color);
             g.FillEllipse(b, X - radius, Y - radius, radius * 2, radius * 2);
 
-            var y2 = Y + speedY;
-            float d = (float)Math.Sqrt(Math.Pow(-speedX,2) + Math.Pow(-speedY, 2));
-            if (d > radius)
-               y2 -= (d-radius);
-            else
-            {
-                y2 = Y+radius;
-            }
+            var y2 = vectorWithoutGravitation();
 
-            g.DrawLine(new Pen(Color.Blue,2), X, Y, X + speedX, y2);
-
+            g.DrawLine(new Pen(Color.Blue,2), X, Y, X + speedX, y2); //Рисовать вектор направления движения
           
             b.Dispose();
         }
         public void watchInfo(Graphics g) //Показать информацию о частице
         {
-            var b = new SolidBrush(Color.FromArgb(124, 252, 0));
+            var b = new SolidBrush(Color.FromArgb(180,Color.White));
 
+            g.FillRectangle(b, X + radius, Y ,120, 60);
+
+            b = new SolidBrush(Color.Black);
             g.DrawString("x = " + X + "\ny = " + Y
-          + "\nLife= " + life, new Font("Times New Roman", 12), b,X + 10, Y);
+          + "\nLife= " + life, new Font("Times New Roman", 12), b,X + radius, Y);
 
             g.DrawEllipse(new Pen(Color.FromArgb(255,0,0), 5), X - radius,
                 Y - radius, radius * 2, radius * 2);
 
             b.Dispose();
         }
+
     }
 }
